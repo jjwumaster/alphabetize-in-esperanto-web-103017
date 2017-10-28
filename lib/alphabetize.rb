fc54259b730853ac7a2eeeb02c4fa376e4f99ddd
github username <github email address>
2017-10-28 17:34:38 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,28 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  dict = {}
+  for i in (1..esperanto_alphabet.length)
+    dict[esperanto_alphabet[i - 1]] = i
+  end
+
+  holder = {}
+
+  arr.each do |word|
+    wordstorage = []
+    for i in (0..word.length - 1)
+      wordstorage << dict[word[i]]
+    end
+    holder[word] = wordstorage
+  end
+
+  arr.sort_by! do |word|
+    holder[word]
+  end
+
+  arr
+
 end
+
+testarray = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
+puts alphabetize(testarray)
